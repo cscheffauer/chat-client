60ae6a4809c9950db6cd6feb5ccca9482ce893a1
Christoph Scheffauer <scheffauerc@gmail.com>
2020-08-17 21:03:14 +0200

    added message input field to chat added handlechange to keep message in component state

--- src/components/chat/Chat.component.tsx
+++ src/components/chat/Chat.component.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { FormEvent, useRef, useState } from 'react';
 
 interface Props {
 	username: string;
 }
 
 const Chat = ({ username }: Props) => {
-	return <div>{username}</div>;
+	const [message, setmessage] = useState('');
+	const messageRef = useRef<HTMLInputElement>(null); //ref for messageRef
+	const handleSubmit = () => {};
+
+	const handleChange = (event: FormEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		setmessage(event.currentTarget.value);
+	};
+
+	return (
+		<div className='chat'>
+			<form onSubmit={handleSubmit}>
+				<input ref={messageRef} placeholder={'Type your message here'} onChange={handleChange} value={message} type='text'></input>
+			</form>
+		</div>
+	);
 };
 
 export default Chat;
