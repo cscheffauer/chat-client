80ddb4ddf99f65bde4757d9de0c952d1c777806f
Christoph Scheffauer <scheffauerc@gmail.com>
2020-08-17 23:46:17 +0200

    Finished styling of chat

--- src/components/Message/Message.component.tsx
+++ src/components/Message/Message.component.tsx
@@ -6,11 +6,22 @@ interface Props {
 }
 
 const Message = ({ username, message }: Props) => {
+	const ts = new Date(message.timestamp);
+	const formatedTimestamp = ts.getDate() + ' ' + ts.toLocaleString('default', { month: 'short' }) + ' ' + ts.getFullYear() + ' ' + ts.getHours() + ':' + ts.getMinutes();
 	return (
-		<div className='message'>
-			<span>
-				{message.author} {message.message} {message.timestamp}
-			</span>
+		<div className='messageLine'>
+			{username !== message.author ? (
+				<div className='message'>
+					<span className='author'>{message.author}</span>
+					<span className='text'>{message.message}</span>
+					<span className='ts'>{formatedTimestamp}</span>
+				</div>
+			) : (
+				<div className='message own'>
+					<span className='text'>{message.message}</span>
+					<span className='ts'>{formatedTimestamp}</span>
+				</div>
+			)}
 		</div>
 	);
 };
