9b0ff037b5f225e2a0b16ec29474c9d903769a75
Christoph Scheffauer <scheffauerc@gmail.com>
2020-08-24 17:38:35 +0200

    Git ignorecase config set to false

---
+++ src/components/Login/Login.component.tsx
@@ -0,0 +1,30 @@
+import React, { FormEvent, useState } from 'react';
+import './Login.styles.scss';
+
+interface Props {
+	loginUser: (username: string) => void;
+}
+
+const Login = ({ loginUser }: Props) => {
+	const [username, setusername] = useState('');
+
+	const changeUsername = (event: FormEvent<HTMLInputElement>) => {
+		event.preventDefault();
+		setusername(event.currentTarget.value);
+	};
+
+	const handleSubmit = (event: FormEvent) => {
+		event.preventDefault();
+		if (username.length > 0) loginUser(username); //check if username is larger than 0 characters - to avoid empty usernames in chat
+	};
+	return (
+		<div className='login'>
+			<form onSubmit={handleSubmit}>
+				<input onChange={changeUsername}></input>
+				<button type='submit'>Join</button>
+			</form>
+		</div>
+	);
+};
+
+export default Login;
